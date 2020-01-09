713a8cc8b5f947ee0ce3c84c04da376c9a73954c
Max Zlotskiy <max.zlotskiy@gmail.com>
2020-01-09 12:50:25 -0500

    added comments

--- src/components/CitiesByZip.js
+++ src/components/CitiesByZip.js
@@ -12,6 +12,11 @@ class CitiesByZipContainer extends Component {
 		this.handleUpdate = this.handleUpdate.bind(this);
 	}
 
+	/**
+	* This method takes data from the search bar child component.
+	* The card children will be update with this data.
+	* @param data  an array of objects with keys cityState, state, population, wages and coordinates.
+	*/
 	handleUpdate(data) {
 		this.setState({cardData: data});
 	}
@@ -19,8 +24,12 @@ class CitiesByZipContainer extends Component {
 	render() {
 		return (
 			<div>
-				<SearchComponent handleSearch={this.handleUpdate}></SearchComponent>
+				{/* The search bar needs to be given a callback function by 
+				the parent, so they can communicate and "lift state up" 
+				when the API responds with data */}
+				<SearchComponent updateParent={this.handleUpdate}></SearchComponent>
 				<div>
+					{/* List of all cards for each piece of data from API */}
 					{this.state.cardData.map((value, index) => (
 						<CityCard key={index} 
 								cityState={value.cityState} 
